5c47a943af81d6ef46d474680e363611bfc5f2bd
gary <gwleong@gmail.com>
2015-06-20 00:41:29 +0000

    added metadata

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'docker-restheart-mongo'
+maintainer       'Gary Leong'
+maintainer_email 'gwleong@gmail.com'
+license          'MIT License'
+description      'to install restheart and mongo'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "docker"
