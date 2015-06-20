8d07487920214d5563bbe046c6d58172d95c90ae
gary <gwleong@gmail.com>
2015-06-20 23:42:04 +0000

    separated out recipes for restheart mongo docker

---
+++ recipes/mongodb.rb
@@ -0,0 +1,27 @@
+#Decided to use a different mongo docker from tutum
+#docker_image 'mongo:2.6' do
+#  retries 3
+#  retry_delay 2
+#  cmd_timeout 900
+#end
+
+docker_image 'tutum/mongodb:2.6' do
+  retries 3
+  retry_delay 2
+  cmd_timeout 900
+end
+
+#Destroy container at every run
+if `sudo docker ps -a | grep mongodb`.size > 0
+  execute('remove container') { command "docker rm -f mongodb" }
+end
+
+docker_container 'mongodb' do
+  container_name 'mongodb'
+  image 'tutum/mongodb:2.6'
+  detach true
+  env ["MONGODB_PASS=#{node['mongodb']['password']}"]
+  port ['27017:27017','28017:28017']
+  volume '/var/lib/mongodb:/data/db'
+end
+
---
+++ recipes/restheart.rb
@@ -0,0 +1,29 @@
+docker_image 'java:8u45-jre' do
+  retries 3
+  retry_delay 2
+  cmd_timeout 900
+end
+
+docker_image 'restheart' do
+  retries 3
+  retry_delay 2
+  source '/var/tmp/docker/restheart'
+  tag 'latest'
+  action :build_if_missing
+  cmd_timeout 900
+end
+
+#Destroy container at every run
+if `sudo docker ps -a | grep restheart`.size > 0
+  execute('remove container') { command "docker rm -f restheart" }
+end
+
+docker_container 'restheart' do
+  image 'restheart:latest'
+  container_name 'restheart'
+  port ['443:443']
+  link ['mongodb:mongodb']
+  detach true
+  action :run
+end
+
